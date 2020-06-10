2fe1b8dab934c7351d124d53ccb21b1c857e414f
Jack Misteli <jgmisteli@gmail.com>
2020-06-10 21:15:28 +0900

    Updates

---
+++ precache-manifest.f5003e7196a24177ab2956258a07838b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917b308960b2d8133855adbb99395727",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e66af599d825b65ec47",
+    "url": "/static/css/main.5748c3eb.chunk.css"
+  },
+  {
+    "revision": "d7c897f8d973e6598db0",
+    "url": "/static/js/2.9cf27ea9.chunk.js"
+  },
+  {
+    "revision": "81e55dd6344646c2e450a0c59ee381b8",
+    "url": "/static/js/2.9cf27ea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e66af599d825b65ec47",
+    "url": "/static/js/main.03ab12bf.chunk.js"
+  },
+  {
+    "revision": "90ff2675d3076eb25635",
+    "url": "/static/js/runtime-main.f634dd95.js"
+  },
+  {
+    "revision": "b4084e44bea589ec3800fdfdd69cf189",
+    "url": "/static/media/icon-check.b4084e44.svg"
+  },
+  {
+    "revision": "6387c00b7d94ad6868cb9308cfef3b15",
+    "url": "/static/media/logo.6387c00b.svg"
+  }
+]);
